8bb0a1d16bfff8d29f137b34a8e1ea2343e40bb4
Tyler Stefanich <tstefanich@Tylers-MacBook-Pro-2.local>
2016-07-11 19:02:17 +0900

    Trash Cron
    I disabled the child process for this just because I imagine this is
    probably a very “simple” operation. We should probably do child
    processes for indexing.

--- mongo/index.js
+++ mongo/index.js
@@ -7,19 +7,14 @@ Schedules will go here for now, if needed we can put them in a schedule.js file
 */
 
 var schedule = require('node-schedule');
-var cp = require('child_process');
+//var cp = require('child_process');
 
 // for testing, run this every 30 sec, might be more efficient to leave child_process running and
 // schedule commands for the process to execute some code
 var j = schedule.scheduleJob('30 * * * * *', function(){
-	var trash = cp.fork('./schedules/trash');
-	
-	// This line will delete files that have not been modified for 30 days or more days
-	// it looks like when we do mv() to move files this changes the last modifited time on 
-	// on files 
-	// find ~/data/trash/* -mtime +30 -exec rm -f {} \; 
-
-	trash.on('close', (code) => console.log(`trash exited w/ code ${code}`));
+	var trash = exec('find ./data/trash/* -mtime +30 -exec rm -f {} \\; ').stdout;
+	//var trash = cp.fork('./schedules/trash');
+	//trash.on('close', (code) => console.log(`trash exited w/ code ${code}`));
 });
 
 
