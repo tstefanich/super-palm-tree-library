6986e0660d90d1da6053c62227e9b4fffeb38c3a
Tyler Stefanich <tstefanich@Tylers-MacBook-Pro-2.local>
2016-07-14 00:36:49 +0900

    Merge remote-tracking branch 'johnbrumley/master'
    # Conflicts:
    #	mongo/index.js

--- mongo/index.js
+++ mongo/index.js
@@ -1,4 +1,4 @@
-var app = require('./app.js');
+// var app = require('./app.js');
 
 /*
 
@@ -11,15 +11,72 @@ var schedule = require('node-schedule');
 
 // for testing, run this every 30 sec, might be more efficient to leave child_process running and
 // schedule commands for the process to execute some code
+
 var j = schedule.scheduleJob('30 * * * * *', function(){
 	var trash = exec('find ./data/trash/* -mtime +30 -exec rm -f {} \\; ').stdout;
 	//var trash = cp.fork('./schedules/trash');
 	//trash.on('close', (code) => console.log(`trash exited w/ code ${code}`));
 });
 
+// var j = schedule.scheduleJob('30 * * * * *', function(){
+// 	var trash = cp.fork('./schedules/trash');
+// 	trash.on('close', (code) => console.log(`trash exited w/ code ${code}`));
+// });
 
 
+/*
+var children = [];
+
+var index = cp.fork(__dirname + '/schedules/index.js');
+
+index.on('message', (response) => console.log(response));
+index.on('close', (code) => console.log(`index exited w/ code ${code}`));
+
+var indexingSchedule = schedule.scheduleJob('10 * * * * *', () => {
+	index.send({index: true, dir: './unindexed-files'})
+>>>>>>> johnbrumley/master
+});
+
+children.push(index); // store in collection
+
+
+
+// app.listen( 8080, function() {  
+//   console.log( 'Express server listening on port 8080' );
+// });
+
+
+
+// dealing with exit now that we've got children, not totally sure how/why all of this
+// is working, if there's a cleaner way to have this happen let me know
+
+function exitHandler(options, err) {
+
+    if (options.cleanup) {
+    	console.log( `\nShutting down ${children.length} process(es)` );
+    	// clean up child processes
+		for(child of children){
+			child.send({exit : true});
+		}
+    }
+    if (err) console.log(err.stack);
+    if (options.exit) process.exit();
+}
+
+//do something when app is closing
+
+<<<<<<< HEAD
 
 app.listen( 8080, function() {  
   console.log( 'Express server listening on port 8080' );
 });
+=======
+// process.exit()
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+// ctrl+c
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+// uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+>>>>>>> johnbrumley/master
+
+*/
--- mongo/schedules/index.js
+++ mongo/schedules/index.js
@@ -1,6 +1,62 @@
-/*
+// require some modules
+var fs = require('fs');
 
-Looking into different cron-like scheduling libs for node,, seems like https://github.com/node-schedule/node-schedule is most actively maintained, though http://bunkat.github.io/later/ seems a bit more intuitive. I'm not sure if these schedulers run other files as separate processes, which I think will be the main reason we would be interested in using something like this
+function index(dir){
+
+	var core = {
+		checkDirectory: function(dir){
+			
+			return promise;
+		},
+		exit: function(){
+			process.exit();
+		}
+	} 
+
+	return {
+		'areThereFilesIn' : function(dir){
+			return core.checkDirectory(dir);
+		},
+		'exit'	: function(){ core.exit() }
+	}
+}
 
 
-*/
+
+function checkDirectory(dir) {
+	fs.readdir(dir, (err, files) => {
+		if(err){
+			reject(err.message);
+		} else {
+			console.log(`found ${files.length} files`);
+			for(file in files){
+				console.log(file);
+				// do something with file
+
+				// update database?
+			}
+			resolve(files);
+		}
+	});
+}
+
+function indexFiles(files){
+	console.log(`indexing ${files.length} files`);
+}
+
+process.on('message', (m) => { 
+	if(m.index){
+		if(typeof m.dir != 'undefined') {
+			p1 = new Promise((resolve, reject) => checkDirectory(m.dir));
+			p1.then((results) => indexFiles(results));
+			p1.catch((err) => console.log(err.message));
+			
+		} else {
+			console.log('please specify a directort for attribute "dir"');
+		}
+	} else if (m.exit) {
+		exit();
+	} else {
+		console.log('unrecognized message passed to child process');
+	}
+});
